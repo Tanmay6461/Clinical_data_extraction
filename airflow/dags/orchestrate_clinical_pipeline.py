ccd14798a1ecfd4fc2505f9b55861be13e6b1bfe
Tanmay6461 <pawar.ta@northeastern.edu>
2025-05-14 04:08:55 -0400

    final

--- airflow/dags/orchestrate_clinical_pipeline.py
+++ airflow/dags/orchestrate_clinical_pipeline.py
@@ -1,9 +1,6 @@
 from airflow import DAG
 from airflow.decorators import task
-from airflow.utils.dates import days_ago
 from utils.data_scrapper import SECFilingExtractor
-from google.oauth2 import service_account
-from airflow.providers.google.cloud.hooks.gcs import GCSHook
 from utils.hleper import get_gcp_credentials_from_airflow, list_gcs_folders
 from airflow.operators.python import PythonOperator
 from datetime import datetime, timedelta
@@ -12,7 +9,9 @@ from airflow.utils.trigger_rule import TriggerRule
 
 credentials = get_gcp_credentials_from_airflow(conn_id="google_cloud_default")
 
+#get all the available tickers from the gcs bucket
 tickers = list_gcs_folders(bucket_name="clinical_data_may25",credentials=credentials)
+
 
 def process_ticker_func(ticker):
     extractor = SECFilingExtractor(ticker=ticker,email="tanmayp@gmail.com", output_dir=f"sec_data/{ticker}")
@@ -27,19 +26,13 @@ default_args = {
     'retry_delay': timedelta(minutes=2)
 }
 
-with DAG("sec_filing_scheduler", schedule_interval="@daily", default_args=default_args, catchup=False) as dag:
-    previous_task = None
-
+#run each dag but one at a time
+with DAG("sec_filing_scheduler", schedule_interval="@daily", default_args=default_args, catchup=False, max_active_tasks=1) as dag:
     for ticker in tickers:
         task = PythonOperator(
             task_id=f"process_{ticker.lower()}",
             python_callable=process_ticker_func,
             op_args=[ticker],
-            retries=3,
-            retry_delay=timedelta(minutes=10),
-            trigger_rule=TriggerRule.ALL_DONE if previous_task else "all_success"
+            retries=2,
+            retry_delay=timedelta(minutes=0.5),
         )
-
-        if previous_task:
-            previous_task >> task
-        previous_task = task
